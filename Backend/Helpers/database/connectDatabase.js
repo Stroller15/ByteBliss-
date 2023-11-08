1ff6dc5e7130081a107fc8f7dc431f37843a72fb
Stroller15 <shubhamverma6351@gmail.com>
2023-11-08 23:06:46 +0530

    fix bug in mongodb connection

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDatabase =async  () => {
 
-    await mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser : true})
+    await mongoose.connect(process.env.MONGO_URI ,{ useNewUrlParser: true, useUnifiedTopology: true })
 
     console.log("MongoDB Connection Successfully")
 

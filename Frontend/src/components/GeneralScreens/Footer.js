f9adc1d962ed0aa4b40a635ee43138e846af9077
Stroller15 <shubhamverma6351@gmail.com>
2023-11-08 16:57:38 +0530

    feat: :sparkles: initial commit

---
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../Css/Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+            </div>
+            <div className="copyright">
+                <p className="copyright-blog">© 2023 Shubham Verma. All Rights Reserved</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
